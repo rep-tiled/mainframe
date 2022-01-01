b7e9a0ccb1081ea6357fe4f596a65c510d10f19d
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-01-01 02:07:38 -0800

    name field

--- scripts/setupVars.js
+++ scripts/setupVars.js
@@ -5,9 +5,11 @@ var cyptoCoins = 0;
 //items
 
 /*load vars from cookies*/
-var savedGame = JSON.parse(localStorage.getItem("mainFrameSave"));
-//player
-if (typeof savedGame.hackerName !== "undefined") hackerName = savedGame.hackerName;
-if (typeof savedGame.cyptoCoins !== "undefined") cyptoCoins = savedGame.cyptoCoins;
+var savedGame = JSON.parse(localStorage.getItem("mainFrameSave"))
+if(savedGame !== null){
+    //player
+    hackerName = savedGame.hackerName
+    cyptoCoins = savedGame.cyptoCoins
+}
 
 //todo: add load game function so the game knows to update visuals
